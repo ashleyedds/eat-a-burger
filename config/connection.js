c3927405db4e980306161fdba822031871a3e211
Ashley Edds <ashley@edds.net>
2018-03-19 12:50:22 -0400

    fix JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
-const connection;
-
-if (process.env.JAWSDB_URL) {
+var connection;
+if(process.env.JAWSDB_URL) {
+  //Heroku deployment
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
